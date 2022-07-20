4ce53530936c5adfeef40a058dcc2a4b08cb24ed
Eugenio <lugezz@gmail.com>
2022-07-20 19:41:13 -0300

    Format change

--- login/views.py
+++ login/views.py
@@ -9,6 +9,7 @@ from django.views.generic.base import RedirectView
 
 from snc import settings
 
+
 class LoginFormView(LoginView):
     template_name = 'login.html'
 
@@ -21,37 +22,38 @@ class LoginFormView(LoginView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context ['title'] = "Iniciar Sesión"
-    
+        context['title'] = "Iniciar Sesión"
         return context
+
 
 class LoginFormView2(FormView):
     form_class = AuthenticationForm
     template_name = 'login.html'
-    success_url = reverse_lazy (settings.LOGIN_REDIRECT_URL) #ver si da error
+    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)  # Ver si da error
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return HttpResponseRedirect (self.success_url)
+            return HttpResponseRedirect(self.success_url)
         return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         login(self.request, form.get_user())
 
-        return HttpResponseRedirect (self.success_url)
+        return HttpResponseRedirect(self.success_url)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context ['title'] = "Iniciar Sesión"
+        context['title'] = "Iniciar Sesión"
         return context
 
 
 class LogoutFormView(LogoutView):
     pass
 
+
 class LoginRedirectView(RedirectView):
     pattern_name = 'login'
 
     def dispatch(self, request, *args, **kwargs):
         logout(request)
-        return super().dispatch(request, *args, **kwargs)   
+        return super().dispatch(request, *args, **kwargs)
